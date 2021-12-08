209a8f8ced94cd349ee3f8182cd2cfcf0846fa32
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2021-12-08 13:28:42 +0000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+# shell.nix
+{ pkgs ? import <nixpkgs> {} }:
+let
+  my-python = pkgs.python3;
+  python-with-my-packages = my-python.withPackages (p: with p; [
+    requests
+    # other python packages you want
+  ]);
+in
+pkgs.mkShell {
+  buildInputs = [
+    python-with-my-packages
+    # other dependencies
+  ];
+  shellHook = ''
+    PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
+    # maybe set more env-vars
+  '';
+}
